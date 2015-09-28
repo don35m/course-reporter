e42842a3d7233ed4c8ba5016d6f1f2d601194cdc
Don Schemmel <dons500@gmail.com>
2015-09-27 20:20:54 -0700

    Add to git

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -1,4 +1,7 @@
-studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope, StudentsFactory) {
-  $scope.students = StudentsFactory.students;
-  $scope.StudentsFactory = StudentsFactory;
+courseRoster.controller('StudentsCtrl', function StudentsCtrl($scope, $stateParams, CoursesFactory, UtilitiesFactory) {
+  $scope.course = UtilitiesFactory.findById(CoursesFactory.courses, $stateParams.courseId)
+  $scope.addStudent = function() {
+    $scope.course.students.push({ name: $scope.studentName });
+    $scope.studentName = null;
+  }
 });
