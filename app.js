10e0b785f479f995c8ac3bc2dce5ddabecc11120
Don Schemmel <dons500@gmail.com>
2015-10-01 19:30:54 -0700

    Add multiple views to header

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
 var courseRoster = angular.module('courseRoster', ['ui.router']);
 
 courseRoster.config(function($stateProvider, $urlRouterProvider) {
+
   $stateProvider.state('home', {
     url: "",
-    templateUrl: "partials/home.html"
+    views: {
+      'header': {
+        templateUrl: "partials/header.html",
+        controller: 'HeadersCtrl',
+      },
+      'body': {
+        templateUrl: "partials/home.html",
+      },
+    }
   });
 
   $stateProvider.state('courses', {
     url: "/courses",
+    views: {
+      'header': {
+        templateUrl: "partials/header.html",
+        controller: 'HeadersCtrl',
+      },
+      'body': {
     templateUrl: "partials/courses.html",
     controller: 'CoursesCtrl'
+      },
+    }
   });
 
   $stateProvider.state('courses.students', {
     url: "/:courseId",
+    views: {
+      'header': {
+        templateUrl: 'partials/header.html',
+        controller: 'HeadersCtrl',
+      },
+      'body': {
     templateUrl: "partials/courses.students.html",
     controller: 'StudentsCtrl'
+      },
+    }
   });
 });
